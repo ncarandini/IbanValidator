3a045fddaa552f28f29f77a27e584a968f6b1aff
Nicolò Carandini <n.carandini@outlook.com>
2020-04-20 17:32:13 +0200

    Add error type to decouple from integer error value

---
+++ TPCWare.IbanValidator/ErrorType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPCWare.IbanValidator
+{
+    public enum ErrorType
+    {
+        None,
+        NotSupportedCountry,
+        WrongCountryFormat,
+        FailedChecksum,
+        InternalError
+    }
+}
